cd966376cda9bd38de7de27baee3581305c3a70f
Florian Pigorsch <florian@concept.de>
2016-12-09 17:30:03 +0100

    part2

--- 09/c++/main1.cpp
+++ 09/c++/main1.cpp
@@ -5,22 +5,23 @@
 int main() {
     const std::regex re{"\\((\\d+)x(\\d+)\\)"};
     std::smatch m;
-    
-    std::string line; std::getline(std::cin, line);
-    int count = 0;
-    auto cend = line.cbegin() + (line.size() - 1);
-    for (auto c = line.cbegin(); c != line.cend(); /**/) {
+    std::string s; std::getline(std::cin, s);
+    std::size_t n = 0;
+    auto cend = s.cbegin() + (s.size() - 1);
+    for (auto c = s.cbegin(); c != s.cend(); /**/) {
         if (*c == '(') {
             std::regex_search(c, cend, m, re);
             c += m.length();
-            c += std::stoi(m[1]);
-            count += std::stoi(m[1]) * std::stoi(m[2]);
+            auto datalength  = static_cast<std::size_t>(std::stoi(m[1]));
+            auto repetitions = static_cast<std::size_t>(std::stoi(m[2]));
+            n += repetitions * datalength;
+            c += datalength;
         } else {
+            ++n;
             ++c;
-            ++count;
         }
     }
      
-    std::cout << "Count: " << count << std::endl;
+    std::cout << "Count: " << n << std::endl;
     return 0;
 }
--- 09/c++/main2.cpp
+++ 09/c++/main2.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <regex>
 #include <string>
 
+std::size_t expand(std::string::const_iterator c, std::size_t len) {
+    static const std::regex re{"\\((\\d+)x(\\d+)\\)"};
+    std::smatch m;
+    std::size_t n = 0;
+    auto cend0 = c + len;
+    auto cend1 = c + (len - 1);
+    while (c != cend0) {
+        if (*c == '(') {
+            std::regex_search(c, cend1, m, re);
+            c += m.length();
+            auto datalength  = static_cast<std::size_t>(std::stoi(m[1]));
+            auto repetitions = static_cast<std::size_t>(std::stoi(m[2]));
+            n += repetitions * expand(c, datalength);
+            c += datalength;
+        } else {
+            ++n;
+            ++c;
+        }
+    }
+    return count;
+}
+
 int main() {
-    std::string line;
-    while (std::getline(std::cin, line)) {
-        
-    }    
+    std::string line; std::getline(std::cin, line);
+    std::cout << "Count: " << expand(line.cbegin(), line.size()) << std::endl;
     return 0;
 }
