6cbb921ad4892dc2c87cb3f126fea1f9d93ddd87
flopp <fpigorsch@gmail.com>
2016-12-18 15:52:33 +0100

    some optimizations

--- 18/c++/main.cpp
+++ 18/c++/main.cpp
@@ -2,25 +2,25 @@
 #include <string>
 
 int main(int /*argc*/, char** argv) {
-    std::string s1, s2; 
-    std::getline(std::cin, s1);
+    std::string a;
+    std::getline(std::cin, a);
     const int rows = std::stoi(argv[1]);
-    const int cols = static_cast<int>(s1.size());
-    
+    const int cols = static_cast<int>(a.size());
+    std::string b(cols, '?');
+
     int safe = 0;
-    for (const auto& c: s1) { safe += (c == '.'); }
-    
-    for (int row = 1; row < rows; ++row) {
-        s2 = (s1[1] == '^') ? '^' : '.';
+    for (int row = 0; row < rows; ++row) {
+        for (const auto& c: a) { safe += (c == '.'); }
+
+        b[0] = (a[1] == '^') ? '^' : '.';
         for (int i = 1; i < cols-1; ++i) {
-            s2 += (s1[i-1] != s1[i+1]) ? '^' : '.';
+            b[i] = (a[i-1] != a[i+1]) ? '^' : '.';
         }
-        s2 += (s1[cols-2] == '^') ? '^' : '.';
-        
-        for (const auto& c: s2) { safe += (c == '.'); }
-        std::swap(s1, s2);
+        b[cols-1] = (a[cols-2] == '^') ? '^' : '.';
+
+        std::swap(a, b);
     }
     std::cout << safe << std::endl;
-    
+
     return 0;
 }
